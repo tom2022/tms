22eaf7394d821e33398d785c96c31997f57cfb5a
tom2022 <32955754+tom2022@users.noreply.github.com>
2019-05-27 17:26:08 +0200

    refactor tour data service

--- src/app/tour-data.service.ts
+++ src/app/tour-data.service.ts
@@ -22,67 +22,20 @@ export class TourDataService {
         return this._tours.asObservable();
     }
 
-    /*const tours_sort = function (tour1, tour2) {
-        const t1 = tour1.tourStartTime.split('T');
-        const t2 = tour2.tourStartTime.split('T');
-        const date1 = t1[0].tourStartTime.split('-');
-        const date2 = t2[0].tourStartTime.split('-');
-        const time1 = t1[1].tourStartTime.split(':');
-        const time2 = t2[1].tourStartTime.split(':');
-        // compare years of tours
-        if (+date1[0] > +date2[0]) {
-            return -1;
-        }
-        if (+date1[0] < +date2[0]) {
-            return 1;
-        }
-        // compare months of tours
-        if (+date1[1] > +date2[1]) {
-            return -1;
-        }
-        if (+date1[1] < +date2[1]) {
-            return 1;
-        }
-        // compare days of tours
-        if (+date1[2] > +date2[2]) {
-            return -1;
-        }
-        if (+date1[2] < +date2[2]) {
-            return 1;
-        }
-        // compare hours of tours
-        if (+time1[0] > +time2[0]) {
-            return -1;
-        }
-        if (+time1[0] < +time2[0]) {
-            return 1;
-        }
-        // compare minutes of tours
-        if (+time1[1] > +time2[1]) {
-            return -1;
-        }
-        if (+time1[1] < +time2[1]) {
-            return 1;
-        }
-        else {
-            return 0;
-        }
-    }*/
-
     fetchTours() {
         return this.http
             .get('https://bpt-lab.org/smile/sphinx/getTours')
             .pipe(
                 map(resData => {
-                    let newTours = [];
+                    const newTours = [];
                     newTours.push(this.testTourService.exampleTour);
-                    for(let tour of resData["tours"]){
-                        const stops = tour["stops"];
-                        const parcels = tour["packets"];
-                        const metaData = tour["tourMetaData"];
-                        let newStops = [];
-                        let newParcels = [];
-                        for(let stop of stops){
+                    for (let tour of resData['tours']) {
+                        const stops = tour['stops'];
+                        const parcels = tour['packets'];
+                        const metaData = tour['tourMetaData'];
+                        const newStops = [];
+                        const newParcels = [];
+                        for (let stop of stops) {
                             newStops.push(new TourStop(
                                 stop.stopType,
                                 stop.id,
@@ -101,15 +54,15 @@ export class TourDataService {
                                 stop.receiverLevel,
                                 stop.receiverRemark,
                                 stop.depotCategory
-                            ))
+                            ));
                         }
-                        for(let parcel of parcels) {
+                        for (let parcel of parcels) {
                             newParcels.push(new Parcel(
                                 parcel.receiverID,
                                 parcel.sscc,
                                 parcel.depotID,
                                 false
-                            ))
+                            ));
                         }
                         newTours.push(new Tour(
                             metaData.tourID,
@@ -119,7 +72,7 @@ export class TourDataService {
                             metaData.price,
                             newStops,
                             newParcels,
-                        ))
+                        ));
                     }
                     newTours.sort(function (tour1, tour2) {
                         const t1 = tour1.tourStartTime.split('T');
@@ -174,17 +127,16 @@ export class TourDataService {
             );
     }
 
-    updateDeliveredParcels(tourID, deliveredParcel){
+    updateDeliveredParcels(tourID, deliveredParcel) {
         return this.tours.pipe(take(1), tap(tours => {
             const tourIndex = tours.findIndex(t => t.tourID === tourID);
             const updatedTours = [...tours];
             const oldTour = updatedTours[tourIndex];
-            let updatedParcels = [];
-            for(let p of oldTour.parcelData){
-                if(p.sscc === deliveredParcel){
-                    updatedParcels.push(new Parcel(p.receiverID, p.sscc, p.depotID,true));
-                }
-                else{
+            const updatedParcels = [];
+            for (let p of oldTour.parcelData) {
+                if (p.sscc === deliveredParcel) {
+                    updatedParcels.push(new Parcel(p.receiverID, p.sscc, p.depotID, true));
+                } else {
                     updatedParcels.push(p);
                 }
             }
@@ -201,17 +153,33 @@ export class TourDataService {
         }));
     }
 
-    updateCompletedStops(tourID, stopID){
+    updateCompletedStops(tourID, stopID) {
         return this.tours.pipe(take(1), tap( tours => {
             const tourIndex = tours.findIndex(t => t.tourID === tourID);
             const updatedTours = [...tours];
             const oldTour = updatedTours[tourIndex];
-            let updatedStops = [];
-            for(let s of oldTour.tourStop){
-                if(s.id === stopID){
-                    updatedStops.push(new TourStop(s.stopType, s.id, s.lastName, s.firstName, s.streetName, s.streetNumber, s.zip, s.city, true, s.startTime, s.endTime, s.plannedTimeframeStart, s.country, s.organization, s.receiverLevel, s.receiverRemark, s.depotCategory));
-                }
-                else{
+            const updatedStops = [];
+            for (let s of oldTour.tourStop) {
+                if (s.id === stopID) {
+                    updatedStops.push(new TourStop(
+                        s.stopType,
+                        s.id,
+                        s.lastName,
+                        s.firstName,
+                        s.streetName,
+                        s.streetNumber,
+                        s.zip,
+                        s.city,
+                        true,
+                        s.startTime,
+                        s.endTime,
+                        s.plannedTimeframeStart,
+                        s.country,
+                        s.organization,
+                        s.receiverLevel,
+                        s.receiverRemark,
+                        s.depotCategory));
+                } else {
                     updatedStops.push(s);
                 }
             }
@@ -226,28 +194,28 @@ export class TourDataService {
             );
             this._tours.next(updatedTours);
             }
-        ))
+        ));
     }
 
     sendParcelDepotHandoverConfirmation(parcelID, date) {
-        let headers = new HttpHeaders();
-        headers.append('Content-Type','application/json');
+        const headers = new HttpHeaders();
+        headers.append('Content-Type', 'application/json');
         return this.http.post('https://bpt-lab.org/smile/caz/tms/pick-up-reported', {
             sscc: parcelID,
             pickDate: date
         }, { headers: headers }).subscribe( (response) => {
             console.log(response);
-        })
+        });
     }
 
     sendParcelReceiverHandoverConfirmation(parcelID, date) {
-        let headers = new HttpHeaders();
-        headers.append('Content-Type','application/json');
+        const headers = new HttpHeaders();
+        headers.append('Content-Type', 'application/json');
         return this.http.post('https://bpt-lab.org/smile/caz/tms/delivery-reported', {
             sscc: parcelID,
             receiveDate: date
         }, { headers: headers }).subscribe( (response) => {
             console.log(response);
-        })
+        });
     }
 }
